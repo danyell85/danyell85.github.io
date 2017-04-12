a5cb1df388a5470050d4cee87f11a26481b748b7
Danielle Cameron <danyell85@gmail.com>
2017-04-12 09:31:35 -0700

    Adding contact section

--- application.js
+++ application.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
-  about()
+  about();
+  contact();
 });
 
 function about(){
   $(".interest").click(function(e){
     $("html,body").animate({ scrollTop: $('.jumbotron').height() }, "slow");
 })
+};
+
+function contact(){
+  $(".contact").click(function(e){
+    $("html,body").animate({ scrollTop: $('.interest').height() }, "slow");
+}
 };
